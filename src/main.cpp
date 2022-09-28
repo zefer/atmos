0ea78af38eb404bb7a8f45738e4d696ca04030a7
Joe Roberts <joe@zefer.co.uk>
2022-09-28 17:05:11 +0100

    Add simple /status JSON webserver endpoint

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,30 @@
 #include <Arduino.h>
 #include <WiFiManager.h>
+#include <WebServer.h>
+
+WebServer server(80);
 
 void configModeCallback (WiFiManager *myWiFiManager) {
   Serial.println("Entered WiFi config mode");
   Serial.println(WiFi.softAPIP());
   Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
+void handleNotFound() {
+  server.send(404, "application/json", "{\"message\":\"Not found\"}");
+}
+
+void handleStatus() {
+  String json;
+  json.reserve(1024);
+  json += "{\"current_time\": ";
+  json += millis();
+  json += ", \"heap_free\":";
+  json += ESP.getFreeHeap();
+  json += "}";
+
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "application/json", json);
 }
 
 void setup() {
@@ -19,7 +39,14 @@ void setup() {
     delay(500);
   }
   Serial.println("WiFi connected");
+
+  server.on("/status", handleStatus);
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
+  server.handleClient();
 }
