f72b69d839074018bf8d8850dbb95794af1d8538
Joe Roberts <joe@zefer.co.uk>
2022-09-28 11:49:22 +0100

    Add some wifi connection debugging/info logging

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <Arduino.h>
 #include <WiFiManager.h>
 
+void configModeCallback (WiFiManager *myWiFiManager) {
+  Serial.println("Entered WiFi config mode");
+  Serial.println(WiFi.softAPIP());
+  Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
 void setup() {
   WiFiManager wifiManager;
+
+  Serial.begin(9600);
+
+  wifiManager.setAPCallback(configModeCallback);
   wifiManager.autoConnect();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  Serial.println("WiFi connected");
 }
 
 void loop() {
