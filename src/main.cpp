888e0a5174210459edbb0322066c2bd41854a0e2
Joe Roberts <joe@zefer.co.uk>
2022-09-29 10:13:34 +0100

    Configure and persist MQTT config vars on first run
    - Use custom fields on the wifimanager UI to allow the user to add mqtt
      config vars
    - Use the preferences lib to persist and read those configured values
      to/from flash storage

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,33 @@
 #include <Arduino.h>
 #include <WiFiManager.h>
 #include <WebServer.h>
+#include <Preferences.h>
 
+Preferences preferences;
 WebServer server(80);
+WiFiClient espClient;
+
+// MQTT server config.
+char defaultMqttServer[40] = "";
+char defaultMqttPort[6] = "1833";
+char defaultMqttNodeName[40] = "atmos-office";
+char defaultMqttPrefix[40] = "atmos";
+String mqttServer;
+String mqttPort;
+String mqttNodeName;
+String mqttPrefix;
+
+bool shouldSaveConfig = false;
 
 void configModeCallback (WiFiManager *myWiFiManager) {
   Serial.println("Entered WiFi config mode");
   Serial.println(WiFi.softAPIP());
   Serial.println(myWiFiManager->getConfigPortalSSID());
+}
+
+void saveConfigCallback () {
+  Serial.println("setting shouldSaveConfig to true");
+  shouldSaveConfig = true;
 }
 
 void handleNotFound() {
@@ -30,15 +50,65 @@ void handleStatus() {
 void setup() {
   WiFiManager wifiManager;
 
+  preferences.begin("testapp1", false);
+
+  // Allow the user to configure MQTT params on the same UI as the WiFi.
+  WiFiManagerParameter mqttServerField("server", "mqtt server", defaultMqttServer, 40);
+  WiFiManagerParameter mqttPortField("port", "mqtt port", defaultMqttPort, 6);
+  WiFiManagerParameter mqttNodeField("nodename", "mqtt node name", defaultMqttNodeName, 40);
+  WiFiManagerParameter mqttPrefixField("prefix", "mqtt prefix", defaultMqttPrefix, 40);
+  wifiManager.addParameter(&mqttServerField);
+  wifiManager.addParameter(&mqttPortField);
+  wifiManager.addParameter(&mqttNodeField);
+  wifiManager.addParameter(&mqttPrefixField);
+
   Serial.begin(9600);
 
   wifiManager.setAPCallback(configModeCallback);
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
   wifiManager.autoConnect();
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
   }
   Serial.println("WiFi connected");
+
+
+  if(shouldSaveConfig) {
+    Serial.println("Writing MQTT config vars");
+    char server[40];
+    char port[6];
+    char node[40];
+    char prefix[40];
+    strcpy(server, mqttServerField.getValue());
+    strcpy(port, mqttPortField.getValue());
+    strcpy(node, mqttNodeField.getValue());
+    strcpy(prefix, mqttPrefixField.getValue());
+    // Persist the user-input MQTT params to flash storage.
+    preferences.putString("mqttServer", server);
+    preferences.putString("mqttPort", port);
+    preferences.putString("mqttNodeName", node);
+    preferences.putString("mqttPrefix", prefix);
+    Serial.println(server);
+    Serial.println(port);
+    Serial.println(node);
+    Serial.println(prefix);
+  }
+
+  // load MQTT config vars from flash storage.
+  Serial.println("Reading MQTT config vars");
+  mqttServer = preferences.getString("mqttServer", defaultMqttServer);
+  mqttPort = preferences.getString("mqttPort", defaultMqttPort);
+  mqttNodeName = preferences.getString("mqttNodeName", defaultMqttNodeName);
+  mqttPrefix = preferences.getString("mqttPrefix", defaultMqttPrefix);
+
+  preferences.end();
+
+  Serial.println("using MQTT prefs...");
+  Serial.println(mqttServer);
+  Serial.println(mqttPort);
+  Serial.println(mqttNodeName);
+  Serial.println(mqttPrefix);
 
   server.on("/status", handleStatus);
   server.onNotFound(handleNotFound);
